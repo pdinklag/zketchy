b7fdc32cf0ceccc37de85dd386a4159c0f0b5e39
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2025-05-12 13:53:14 +0200

    topk-lz77

---
+++ include/zk/topk_lz77.hpp
@@ -0,0 +1,262 @@
+#pragma once
+
+#include <lz77/lpf_factorizer.hpp>
+
+#include "internal/benchmark.hpp"
+#include "internal/io/block_coding.hpp"
+#include "internal/sketch/topk_prefixes_misra_gries.hpp"
+
+namespace zk {
+
+class TopkLZ77 {
+private:
+    static constexpr uint64_t MAGIC =
+        ((uint64_t)'T') << 56 |
+        ((uint64_t)'O') << 48 |
+        ((uint64_t)'P') << 40 |
+        ((uint64_t)'K') << 32 |
+        ((uint64_t)'F') << 24 |
+        ((uint64_t)'A') << 16 |
+        ((uint64_t)'C') << 8 |
+        ((uint64_t)'T');
+
+    static constexpr internal::TokenType TOK_TRIE_REF = 0;
+    static constexpr internal::TokenType TOK_FACT_SRC = 1;
+    static constexpr internal::TokenType TOK_FACT_LEN = 2;
+    static constexpr internal::TokenType TOK_LITERAL = 3;
+    static constexpr internal::TokenType TOK_FACT_REMAINDER = 4;
+
+    static void setup_encoding(internal::BlockEncodingBase& enc, size_t const k, size_t const window_size) {
+        enc.register_binary(k-1);                // TOK_TRIE_REF
+        enc.register_binary(window_size-1);      // TOK_FACT_SRC
+        enc.register_huffman();                  // TOK_FACT_LEN
+        enc.register_binary(255, false);         // TOK_FACT_LITERAL
+        enc.register_binary(window_size, false); // TOK_FACT_REMAINDER
+    }
+    
+    static constexpr size_t MAX_LZ_REF_LEN = 255;
+
+    using Index = uint32_t;
+    using Node = Index;
+    using Topk = internal::TopKPrefixesMisraGries<>;
+
+    size_t threshold_;
+    size_t k_;
+    size_t window_size_;
+    size_t max_freq_;
+    size_t block_size_;
+
+    internal::Result result_;
+
+public:
+    TopkLZ77(size_t const threshold, size_t const k, size_t const window_size, size_t const max_freq, size_t const block_size)
+        : threshold_(threshold),
+          k_(k),
+          window_size_(window_size),
+          max_freq_(max_freq),
+          block_size_(block_size) {
+    }
+
+    TopkLZ77(TopkLZ77&&) = default;
+    TopkLZ77& operator=(TopkLZ77&&) = default;
+    TopkLZ77(TopkLZ77 const&) = default;
+    TopkLZ77& operator=(TopkLZ77 const&) = default;
+
+    template<iopp::STLInputStreamLike InputStream, iopp::BitSink Out>
+    void compress(InputStream& in, Out out) {
+        // init stats
+        internal::MemoryTimePhase stats("topk-lz77");
+        internal::TimePhase phase_read;
+        internal::TimePhase phase_block_lz77;
+        internal::TimePhase phase_process;
+        result_ = {};
+
+        size_t num_lz = 0;
+        size_t num_trie = 0;
+        size_t num_literal = 0;
+        size_t trie_longest = 0;
+        size_t lz_longest = 0;
+        size_t total_trie_len = 0;
+        size_t total_lz_len = 0;
+        size_t num_relevant = 0;
+
+        stats.start();
+
+        // write header and initialize encoding
+        out.write(MAGIC, 64);
+        out.write(k_, 64);
+        out.write(window_size_, 64);
+        out.write(max_freq_, 64);
+
+        internal::BlockEncoder enc(out, block_size_);
+        setup_encoding(enc, k_, window_size_);
+
+        // initialize top-k
+        Topk topk(k_ - 1, max_freq_);
+
+        // initialize factorizer
+        lz77::LPFFactorizer lpf;
+        lpf.min_reference_length(threshold_);
+        std::vector<lz77::Factor> factors;
+
+        // initialize buffers
+        auto block_offs = 0;
+        auto block = std::make_unique<char[]>(window_size_);
+
+        while(in) {
+            // read next block
+            Index block_num;
+            {
+                phase_read.resume();
+                in.read(block.get(), window_size_);
+                block_num = in.gcount();
+                phase_read.pause();
+            }
+
+            if(block_num == 0) {
+                continue;
+            }
+
+            // compute the LZ77 factorization of the block
+            {
+                phase_block_lz77.resume();
+                factors.clear();
+                lpf.factorize(block.get(), block.get() + block_num, std::back_inserter(factors));
+                phase_block_lz77.pause();
+            }
+
+            // parse and encode the block
+            // at the beginning of each LZ77 factor, we attempt to find the longest possible string back in the top-k trie
+            // if we find a string longer than the next LZ77 factor, we encode it using a trie reference and advance in the LZ77 factorization, potentially chopping
+            // the factor that we reach into two fractions
+            {
+                phase_process.resume();
+
+                auto topk_enter = [&](size_t const pos, size_t const len){
+                    ++num_relevant;
+
+                    typename Topk::StringState s = topk.empty_string();
+                    Node node;
+                    while(s.frequent && s.len < len && pos + s.len < block_num) {
+                        node = s.node;
+                        s = topk.extend(s, block[pos + s.len]);
+                    }
+                };
+
+                Index z = 0; // the current LZ77 factor
+                Index curpos = 0;
+                while(curpos < block_num) {
+                    auto const gpos = block_offs + curpos;
+
+                    // find the longest string represented in the top-k trie starting at the current position
+                    Node v;
+                    Index dv = topk.find(block.get() + curpos, block_num - curpos, v);
+
+                    auto const& f = factors[z];
+                    if(dv >= f.num_literals()) {
+                        // encode a top-k trie reference
+                        assert(v > 0);
+
+                        enc.write_uint(TOK_FACT_LEN, 0);
+                        enc.write_uint(TOK_TRIE_REF, v);
+                        
+                        ++num_trie;
+                        trie_longest = std::max(trie_longest, (size_t)dv);
+                        total_trie_len += dv;
+
+                        // advance in LZ77 factorization
+                        if(curpos + dv < block_num) {
+                            auto d = dv;
+                            while(d >= factors[z].num_literals()) {
+                                d -= factors[z].num_literals();
+                                ++z;
+                            }
+
+                            if(d > 0) {
+                                // chop current LZ77 factor
+                                assert(factors[z].len > d);
+                                factors[z].len -= d;
+                            }
+                        }
+
+                        // enter
+                        topk_enter(curpos, dv);
+                        curpos += dv;
+                    } else {
+                        // encode a LZ77 reference or a literal
+                        if(f.is_literal() || f.num_literals() == 1) {
+                            // a literal factor (possibly a reference of length one introduced due to chopping)
+                            enc.write_uint(TOK_FACT_LEN, 1);
+                            enc.write_char(TOK_LITERAL, block[curpos]);
+
+                            ++num_literal;
+
+                            topk_enter(curpos, 1);
+                            ++curpos;
+                        } else {
+                            // a real LZ77 reference
+                            auto const fpos = curpos;
+                            assert(fpos >= f.src);
+
+                            if(f.len >= MAX_LZ_REF_LEN) {
+                                // encode the maximum length, then encode the rest as a special token
+                                enc.write_uint(TOK_FACT_LEN, MAX_LZ_REF_LEN);
+                                enc.write_uint(TOK_FACT_REMAINDER, f.len - MAX_LZ_REF_LEN);
+                            } else {
+                                // simply encode the length
+                                enc.write_uint(TOK_FACT_LEN, f.len);
+                            }
+
+                            // write source
+                            enc.write_uint(TOK_FACT_SRC, f.src);
+
+                            ++num_lz;
+                            lz_longest = std::max(lz_longest, (size_t)f.len);
+                            total_lz_len += f.len;
+
+                            // enter
+                            topk_enter(curpos, f.len);
+                            curpos += f.len;
+                        }
+
+                        // advance to next LZ77 factor
+                        ++z;
+                    }
+                }
+                phase_process.pause();
+            }
+
+            block_offs += block_num;
+        }
+
+        phase_process.resume();
+        enc.flush();
+        phase_process.pause();
+
+        stats.stop();
+
+        result_.add("z", num_lz + num_literal + num_trie);
+        result_.add("phrases_ref", num_lz + num_trie);
+        result_.add("phrases_literal", num_literal);
+        result_.add("num_relevant", num_relevant);
+        result_.add("phrases_longest", std::max(trie_longest, lz_longest));
+        result_.add("phrases_longest_lz", lz_longest);
+        result_.add("phrases_longest_trie", trie_longest);
+        result_.add("phrases_ref_lz", num_lz);
+        result_.add("phrases_ref_trie", num_trie);
+        result_.add("phrases_avg_ref_len", std::round(100.0 * ((double)(total_lz_len + total_trie_len) / (double)(num_lz + num_trie))) / 100.0);
+        result_.add("phrases_avg_ref_len_lz", std::round(100.0 * ((double)total_lz_len / (double)num_lz)) / 100.0);
+        result_.add("phrases_avg_ref_len_trie", std::round(100.0 * ((double)total_trie_len / (double)num_trie)) / 100.0);
+        result_.add("t", stats.get_metric<pm::Stopwatch::ElapsedTimeMillisMetric>());
+        result_.add("t_read", phase_read.get_metric<pm::Stopwatch::ElapsedTimeMillisMetric>());
+        result_.add("t_lz77", phase_block_lz77.get_metric<pm::Stopwatch::ElapsedTimeMillisMetric>());
+        result_.add("t_process", phase_process.get_metric<pm::Stopwatch::ElapsedTimeMillisMetric>());
+        result_.add("mem_peak", stats.get_metric<pm::MallocCounter::MemoryPeakMetric>());
+    }
+
+    auto&& consume_last_result() {
+        return std::move(result_);
+    }
+};
+
+}
---
+++ src/tools/topk_lz77_tool.cpp
@@ -0,0 +1,71 @@
+#include <zk/topk_lz77.hpp>
+#include <zk/internal/util/si_iec_literals.hpp>
+
+#include <oocmd.hpp>
+#include <iopp/bitwise_io.hpp>
+#include <iopp/file_input_stream.hpp>
+#include <iopp/file_output_stream.hpp>
+
+class TopkLZ77Tool : public oocmd::ConfigObject {
+private:
+    std::string output_filename;
+    bool decompress = false;
+
+    uint64_t block_size = 32_Ki; // best value according to many many experiments
+    uint64_t prefix = UINTMAX_MAX;
+    uint64_t k = 1_Mi;
+    uint64_t max_freq = 1_Ki;
+    uint64_t window_size = 1_Mi;
+    unsigned int threshold = 2;
+
+public:
+    TopkLZ77Tool() : oocmd::ConfigObject("Top-k LZ77", "Best of both worlds approach to blockwise LZ77 and top-k LZ78.") {
+        param('o', "out", output_filename, "The output filename.");
+        param('d', "decompress", decompress, "Decompress the input file rather than compressing it.");
+        param('b', "block-size", block_size, "The block size for encoding.");
+        param('p', "prefix", prefix, "The prefix of the input file to consider.");
+        param('k', "num-frequent", k, "The number of frequent substrings to maintain.");
+        param('c', "max-freq", max_freq, "The maximum frequency of a frequent pattern.");
+        param('w', "window", window_size, "The window size.");
+        param('t', "threshold", threshold, "The minimum LZ77 reference length");
+    }
+
+    int run(oocmd::Application const& app) {
+        if(!app.args().empty()) {
+            auto const& filename = app.args()[0];
+            if(decompress) {
+                
+
+            } else {
+                size_t const n = std::min(std::filesystem::file_size(filename), prefix);
+
+                if(output_filename.empty()) {
+                    output_filename = filename + ".topklz77";
+                }
+
+                zk::TopkLZ77 topk_lz77(threshold, k, window_size, max_freq, block_size);
+                {
+                    iopp::FileInputStream in(filename, 0, n);
+                    iopp::FileOutputStream out(output_filename);
+                    topk_lz77.compress(in, iopp::bitwise_output_to(out));
+                }
+
+                auto result = topk_lz77.consume_last_result();
+                result.add("algo", "topk_lz77");
+                result.add("file", std::filesystem::path(filename).filename().string());
+                result.add("n", n);
+                result.sort();
+                result.print();
+            }
+            return 0;
+        } else {
+            app.print_usage(*this);
+            return -1;
+        }
+    }
+};
+
+int main(int argc, char** argv) {
+    TopkLZ77Tool app;
+    return oocmd::Application::run(app, argc, argv);
+}
