2cb8df04b98a108dcd97aee892307c0dcc389758
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2025-09-23 10:34:59 +0200

    Rearrangement of dynamic blocks

--- src/tools/rearrange_tool.cpp
+++ src/tools/rearrange_tool.cpp
@@ -1,21 +1,77 @@
-#include <zk/cscore.hpp>
-#include <zk/internal/util/complete_graph.hpp>
+#include <bit>
 
-#include <oocmd.hpp>
+#include <zk/internal/io/overlapping_blocks.hpp>
+#include <zk/internal/util/complete_graph.hpp>
+#include <zk/internal/util/si_iec_literals.hpp>
+
+#include <code/binary.hpp>
+#include <fp/rk31.hpp>
 #include <iopp/bitwise_io.hpp>
 #include <iopp/file_input_stream.hpp>
 #include <iopp/file_output_stream.hpp>
-#include <code/binary.hpp>
+#include <oocmd.hpp>
+#include <pm/result.hpp>
 
 class RearrangeTool : public oocmd::ConfigObject {
 private:
-    uint64_t sampling = 8;
-    uint64_t len = 8;
-    size_t block_size = 64_Ki;
-    size_t buffer_size = 32_Mi;
+    using Fingerprint = fp::RabinKarp31::Fingerprint;
+    static constexpr Fingerprint fp_base_ = 257;
+
+    size_t sampling = 16_Ki;
+    size_t len = 8;
+    size_t max_minimizers = 64;
+    size_t buffer_size = 64_Mi;
     size_t prefix = SIZE_MAX;
     bool verbose = false;
     std::string output_filename;
+
+    struct Sample {
+        size_t index;
+        uint32_t len;
+
+        std::unique_ptr<Fingerprint[]> minimizers;
+        uint32_t num_minimizers;
+
+        Sample(size_t const _index, size_t const max_minimizers) : index(_index), len(0), minimizers(std::make_unique<Fingerprint[]>(max_minimizers)), num_minimizers(0) {
+        }
+
+        void insert(Fingerprint const fp, size_t const max) {
+            if(num_minimizers == max && fp > minimizers[num_minimizers])[[unlikely]] return;
+
+            size_t rank = 0;
+            while(rank < num_minimizers && minimizers[rank] < fp) {
+                ++rank;
+            }
+
+            if(num_minimizers < max) {
+                ++num_minimizers;
+            }
+
+            for(size_t i = num_minimizers; i > rank; i--) {
+                minimizers[i] = minimizers[i-1];
+            }
+            minimizers[rank] = fp;
+        }
+
+        double similarity(Sample const& other) const {
+            size_t cut_size = 0;
+            size_t i = 0, j = 0;
+            while(i < num_minimizers && j < other.num_minimizers) {
+                if(minimizers[i] < other.minimizers[j]) {
+                    ++i;
+                } else if(minimizers[i] > other.minimizers[j]) {
+                    ++j;
+                } else {
+                    ++cut_size;
+                    ++i;
+                    ++j;
+                }
+            }
+
+            size_t const union_size = num_minimizers + other.num_minimizers - cut_size;
+            return double(cut_size) / double(union_size);
+        }
+    };
 
     struct Edge {
         uint32_t target;
@@ -24,9 +80,9 @@ private:
 
 public:
     RearrangeTool() : oocmd::ConfigObject("Rearrange", "Rearrange blocks of the input according to their similarity") {
-        param('s', "sample", sampling, "The sampling rate (2^value).");
+        param('s', "sample", sampling, "The sampling rate.");
         param('l', "len", len, "The pattern length.");
-        param('b', "block_size", block_size, "The block size.");
+        param('m', "minimizers", max_minimizers, "The maximum number of minimizers per sample.");
         param('w', "buffer_size", buffer_size, "The buffer size.");
         param('p', "prefix", prefix, "Process only this prefix of the input file.");
         param('v', "verbose", verbose, "Print a lot of info.");
@@ -34,36 +90,99 @@ public:
     }
 
     int run(oocmd::Application const& app) {
-        if((buffer_size % block_size) != 0) {
-            std::cerr << "buffer size must be a multiple of block size" << std::endl;
-            return -1;
-        }
-
         if(!app.args().empty()) {
             auto const& filename = app.args()[0];
             size_t const n = std::min(std::filesystem::file_size(filename), prefix);
 
-            if((n + block_size - 1) / block_size > UINT32_MAX) {
-                std::abort();
-            }
-
             iopp::FileInputStream fis(filename, 0, n);
 
-            // compute block signatures
-            std::cout << "compute block signatures ..." << std::endl;
-            zk::CScore cscore(len, sampling, block_size, buffer_size);
-            auto sig = cscore.compute_block_signatures(fis);
-            auto const num_blocks = sig.size();
+            // sample
+            std::cout << "scan ..." << std::endl;
+
+            size_t const min_sample_len = std::bit_floor(sampling / 3); // rate * (1/3)
+            size_t const max_sample_len = 9 * min_sample_len; // rate * 3
+
+            if(verbose) {
+                std::cout << "\tmin_sample_len=" << min_sample_len << std::endl;
+                std::cout << "\tmax_sample_len=" << max_sample_len << std::endl;
+            }
+
+            std::vector<Sample> samples;
+            {
+                Fingerprint const s = 2 * min_sample_len - 1; // rate * (2/3)
+
+                // initialize I/O
+                zk::internal::OverlappingBlocks block(fis, buffer_size, len);
+                if(block.size() < len) std::abort();
+                char const* p = block.begin();
+
+                // initialize fingerprinting
+                fp::RabinKarp31 rk(fp_base_, len);
+                Fingerprint fp = 0;
+
+                size_t i = 0;
+                for(; i < len; i++) {
+                    fp = rk.push(fp, *p++);
+                }
+
+                // initialize samples
+                samples.emplace_back(0, max_minimizers);
+                samples.back().insert(fp, max_minimizers);
+
+                // process blocks
+                size_t next_allowed = min_sample_len;
+                size_t next_mandatory = max_sample_len;
+
+                do {
+                    if(block.empty()) continue;
+
+                    while(p < block.end()) {
+                        // read next character
+                        fp = rk.roll(fp, *(p - len), *p);
+
+                        if(((fp & s) == 0 && i >= next_allowed) || i >= next_mandatory) {
+                            // finalize previous sample
+                            size_t const new_beg = i - len + 1;
+                            samples.back().len = new_beg - samples.back().index;
+                            samples.emplace_back(new_beg, max_minimizers);
+
+                            next_allowed = new_beg + min_sample_len;
+                            next_mandatory = new_beg + max_sample_len;
+                        }
+                        samples.back().insert(fp, max_minimizers);
+
+                        // advance character
+                        ++i;
+                        ++p;
+                    }
+
+                    // advance block
+                    block.advance();
+                    p = block.begin();
+                } while(!block.last());
+
+                samples.back().len = n - samples.back().index;
+            }
+
+            if(verbose) {
+                std::cout << "Samples:" << std::endl;
+                for(auto& x : samples) {
+                    std::cout << "\tindex=" << x.index << ", len=" << x.len << ", minimizers=" << x.num_minimizers << std::endl;
+                }
+            }
+
+            auto const num_samples = samples.size();
 
             // construct graph
-            std::cout << "construct graph (num_blocks=" << num_blocks << ") ..." << std::endl;
-            zk::internal::CompleteGraph g(num_blocks);
-            for(size_t i = 0; i < num_blocks; i++) {
+            std::cout << "construct graph (num_samples=" << num_samples << ") ..." << std::endl;
+            zk::internal::CompleteGraph g(num_samples);
+            for(size_t i = 0; i < num_samples; i++) {
                 g.dist(i, i) = 0.0f;
-                for(size_t j = i+1; j < num_blocks; j++) {
-                    float const sim = float(sig[i].similarity(sig[j]));
-                    g.dist(j, i) = sim;
-                    g.dist(i, j) = sim;
+                for(size_t j = i+1; j < num_samples; j++) {
+                    float const sim = float(samples[i].similarity(samples[j]));
+                    float const d = 1.0f - sim;
+                    g.dist(j, i) = d;
+                    g.dist(i, j) = d;
                 }
             }
 
@@ -71,14 +190,14 @@ public:
             // compute APSP -- SLOW!
             /*
             g.all_pairs_shortest_paths();
-            */
             
             // convert similary to cost
-            for(size_t i = 0; i < num_blocks; i++) {
-                for(size_t j = 0; j < num_blocks; j++) {
+            for(size_t i = 0; i < num_samples; i++) {
+                for(size_t j = 0; j < num_samples; j++) {
                     g.dist(i, j) = 1.0f - g.dist(i, j);
                 }
             }
+            */
 
             // compute TSP tour via Christofides
             std::cout << "compute TSP tour ..." << std::endl;
@@ -112,15 +231,18 @@ public:
                     };
 
                     write_uint32(tour.size());
-                    for(auto v : tour) write_uint32(v);
+                    for(auto v : tour) {
+                        write_uint32(v);
+                        write_uint32(samples[v].len);
+                    }
 
                     // rearrange
-                    auto buffer = std::make_unique<char[]>(block_size);
+                    auto buffer = std::make_unique<char[]>(max_sample_len);
                     for(auto v : tour) {
-                        auto const offs = v * block_size;
+                        auto const offs = samples[v].index;
                         fis.seekg(offs, std::ios::beg);
-                        fis.read(buffer.get(), block_size);
-                        fos.write(buffer.get(), block_size);
+                        fis.read(buffer.get(), samples[v].len);
+                        fos.write(buffer.get(), samples[v].len);
                     }
                     // done
                 }
